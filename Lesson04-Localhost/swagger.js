f0d5634d676bf80c141af2da9fc5fcc6fe8d1b15
Antonio-David-Tejada-Villalon <4vdel777@gmail.com>
2023-01-15 01:44:30 -0300

    Fourth Commit with L04-localhost

--- Lesson04-Localhost/swagger.js
+++ Lesson04-Localhost/swagger.js
@@ -5,8 +5,8 @@ const doc = {
         title: 'My API',
         description: 'Contacts API'
     },
-    host: 'cse341-node-ndyo.onrender.com',
-    schemes: ['https']
+    host: 'localhost:3000',
+    schemes: ['http']
 };
 
 const outputFile = './swagger.json';
